8e81e9e4d4d1c997ac836ad17bc409d334a3fc59
Awashhh <dddrag734@gmail.com>
2024-05-05 19:12:01 +0300

    Starrt

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const path = require("path");
+
+const errorHandler = require("./middlewares/errorHandler");
+const mainRoute = require("./routes/main");
+const searchRoute = require("./routes/search");
+
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(
+  express.json(),
+  errorHandler,
+  express.static(path.join(__dirname, "public")),
+  mainRoute,
+  searchRoute
+);
+
+app.listen(PORT, () =>
+  console.log(`Сервер запущен на http://localhost:${PORT}`)
+);
+
+module.exports = PORT
